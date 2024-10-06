fcae63b7235c0707d1f4693e3bbe26e5987dc546
Shamim2601 <shamim2601@gmail.com>
2024-10-06 23:23:31 +0600

    jsonrenderer

--- backend/settings.py
+++ backend/settings.py
@@ -108,7 +108,10 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 }
 
 
